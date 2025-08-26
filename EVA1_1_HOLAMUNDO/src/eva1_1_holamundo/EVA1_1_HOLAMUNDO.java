0ccfba95f640f4704b00395b997dfdb15353cf41
Jesus-Aranda <arandacardenasjesushumberto@gmail.com>
2025-08-25 18:54:43 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,11 @@
+
+package eva1_1_holamundo;
+
+public class EVA1_1_HOLAMUNDO {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+        System.out.println("FUNDAMENTOS DE PROGRAMACION");
+       
+    }
+    
+}
