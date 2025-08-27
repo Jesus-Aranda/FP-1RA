bbe82d59e7a7ce0d58f1ef2d5bfe5198933a366d
Jesus-Aranda <arandacardenasjesushumberto@gmail.com>
2025-08-27 17:49:42 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,19 @@
+package eva1_5_variables;
+public class EVA1_5_VARIABLES {
+    public static void main(String[] args) {
+        //Declaracion de Variables
+        //Crear las variables para que se puedan usar
+        //Tipo de dato / identificador
+        String name;
+        byte age;
+        //Inicialización
+        //Usamos el operador = --> OPERADOR DE ASIGNACIÓN
+        name = "Jesus Aranda";
+        age = 18;
+        
+        //Uso de Variables
+        System.out.println(name);
+        System.out.println(age);
+        
+    }
+}
